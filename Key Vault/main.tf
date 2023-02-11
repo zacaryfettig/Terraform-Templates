141013fbbe98efea9225f86d5ae2431536953368
zacaryfettig <115122598+zacaryfettig@users.noreply.github.com>
2023-02-11 08:39:24 -0700

    completing resource config

---
+++ Key Vault/main.tf
@@ -0,0 +1,43 @@
+resource "azurerm_resource_group" "resourceGroup" {
+    name = var.resourceGroup
+    location = var.location
+}
+
+//keyvautl resources
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "keyVault" {
+  name                        = "keyVault-${var.resourceGroupName.id}"
+  location                    = azurerm_resource_group.resourceGroup.location
+  resource_group_name         = azurerm_resource_group.resourceGroup.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+      "List",
+    ]
+
+  }
+}
+
+resource "azurerm_key_vault_secret" "vaultSecret" {
+  name         = var.secretName
+  value        = var.secret
+  key_vault_id = azurerm_key_vault.keyVault.id
+  depends_on = [
+    azurerm_key_vault.keyVault
+  ]
+}
---
+++ Key Vault/variables.tf
@@ -0,0 +1,15 @@
+variable "resourceGroup" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "secretName" {
+  type = string
+}
+
+variable "secret" {
+  type = string
+}
