ba60f61779fc7140a3732a3ca6e1418f531a24f8
zacaryfettig <115122598+zacaryfettig@users.noreply.github.com>
2023-04-29 12:25:04 -0600

    Update main.tf

--- Azure Virtual Wan/main.tf
+++ Azure Virtual Wan/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "resourceGroup" {
     location = var.location
 }
 
-#virtual wan
+####################virtual wan####################
 resource "azurerm_virtual_wan" "vwan1" {
   name                = "vwan1"
   resource_group_name = azurerm_resource_group.resourceGroup.name
@@ -19,28 +19,23 @@ resource "azurerm_virtual_hub" "vWanHub1" {
   address_prefix      = "10.3.1.0/24"
 }
 
-resource "azurerm_virtual_hub_connection" "hubConnection1" {
-  name                      = "hubConnection1"
-  virtual_hub_id            = azurerm_virtual_hub.vWanHub1.id
-  remote_virtual_network_id = azurerm_virtual_network.vnet1.id
-}
 
-#virtual network
-resource "azurerm_virtual_network" "vnet1" {
-  name                = "vnet1"
+
+####################First Site To Site VPN Config####################
+resource "azurerm_virtual_network" "localVpnVnet" {
+  name                = "localVpnSubnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.resourceGroup.location
   resource_group_name = azurerm_resource_group.resourceGroup.name
 }
 
-resource "azurerm_subnet" "subnet1" {
-  name                 = "subnet1"
+resource "azurerm_subnet" "localVpnSubnet" {
+  name                 = "localVpnSubnet"
   resource_group_name  = azurerm_resource_group.resourceGroup.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-#Site to Site VPN
 resource "azurerm_vpn_gateway" "vpnGateway1" {
   name                = "vpnGateway1"
   location            = azurerm_resource_group.resourceGroup.location
@@ -58,8 +53,14 @@ resource "azurerm_vpn_site" "vpnPhysicalSite" {
   address_cidrs = ["10.100.0.0/24"]
   link {
     name       = "Office-Link-1"
-    ip_address = "10.1.0.0"
+    ip_address = var.Public_IP_of_Remote_Gateway
   }
+}
+
+resource "azurerm_virtual_hub_connection" "vpnHubConnection1" {
+  name                      = "vpnHubConnection1"
+  virtual_hub_id            = azurerm_virtual_hub.vWanHub1.id
+  remote_virtual_network_id = azurerm_virtual_network.localVpnVnet.id
 }
 
 resource "azurerm_vpn_gateway_connection" "gatewayConnection1" {
@@ -71,4 +72,46 @@ resource "azurerm_vpn_gateway_connection" "gatewayConnection1" {
     name             = "link1"
     vpn_site_link_id = azurerm_vpn_site.vpnPhysicalSite.link[0].id
   }
+}
+
+####################Create and link two vnets to the hub####################
+
+resource "azurerm_virtual_hub_connection" "vnet2Connection" {
+  name                      = "vnet2Connection"
+  virtual_hub_id            = azurerm_virtual_hub.vWanHub1.id
+  remote_virtual_network_id = azurerm_virtual_network.vnet2.id
+}
+
+resource "azurerm_virtual_network" "vnet1" {
+  name = "vnet1"
+  location = var.location
+  resource_group_name = azurerm_resource_group.resourceGroup.name
+  address_space = [ "10.2.0.0/16" ]
+}
+
+resource "azurerm_subnet" "vnet1_subnet1" {
+  name = "vnet1_subnet1"
+  resource_group_name = azurerm_resource_group.resourceGroup.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes = [ "10.2.1.0/24" ]
+}
+
+resource "azurerm_virtual_network" "vnet2" {
+  name = "vnet2"
+  location = var.location
+  resource_group_name = azurerm_resource_group.resourceGroup.name
+  address_space = [ "10.4.0.0/16" ]
+}
+
+resource "azurerm_subnet" "vnet2_subnet1" {
+  name = "vnet2_subnet1"
+  resource_group_name = azurerm_resource_group.resourceGroup.name
+  virtual_network_name = azurerm_virtual_network.vnet2.name
+  address_prefixes = [ "10.4.1.0/24" ]
+}
+
+resource "azurerm_virtual_hub_connection" "vnet1Connection" {
+  name                      = "vnet1Connection"
+  virtual_hub_id            = azurerm_virtual_hub.vWanHub1.id
+  remote_virtual_network_id = azurerm_virtual_network.vnet1.id
 }
